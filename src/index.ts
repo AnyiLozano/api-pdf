856d57e5694f25ab954ea1cafb2be0a08cb46076
AnyiLozano <anyi.lozano2119@gmail.com>
2021-12-27 16:43:51 -0500

    Changes Pdf

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,50 @@
-import express, { Application, Request, Response } from "express";
+import { Response, Request } from 'express';
+import fs from "fs";
 
-const app: Application = express();
+const pdf = require("html-pdf");
 
-app.set("port", 3000);
+export default async(req: Request, res: Response) => {
+  const name: string = typeof req.query.name === "string" ? req.query.name : "";
+  const type = typeof req.query.type === "string" ? req.query.type : "";
+  const id = typeof req.query.id === "string" ? req.query.id : "";
 
-app.get("/", (_req: Request, res: Response) => {
-  res.json({ message: "Hello world!" });
-});
+    const html = fs.readFileSync(__dirname + "/views/index.html", "utf8");
 
-app.listen(app.get("port"), () => {
-  console.log(`Server on http://localhost:${app.get("port")}/`);
-});
+    const reHtml = html.replace('{{userName}}', name)
+                       .replace('{{document_type}}', type)
+                       .replace('{{document}}', id);
+
+    pdf.create(reHtml, {
+        "type": "PDF",
+        "renderDelay": 2000,
+        "orientation": "landscape",
+        "base": "file:///" + __dirname + "\\views\\images\\",
+    })
+        .toStream((err: any, stream: any) => {
+            //    stream.pipe(fs.createWriteStream('../pdf/certification.pdf'))
+            if (err) {
+                return res.send({
+                    status: 500,
+                    msg: "error"
+                })
+            } else {
+                res.statusCode = 200;
+
+                stream.on('end', () => {
+                    return res.end();
+                })
+
+                stream.pipe(res);
+            }
+        })
+}
+
+
+// const Server = () => {
+//   router.get('/', (req: Request, res: Response) => {
+//     console.log(req.params)
+//     res.json(req.params)
+//   })
+// }
+
+// export default Server;
