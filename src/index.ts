5109f036b59bebebd28e3a0ca98df73f49a4f1f9
AnyiLozano <anyi.lozano2119@gmail.com>
2021-12-27 16:47:53 -0500

    Cahnges

--- src/index.ts
+++ src/index.ts
@@ -4,39 +4,40 @@ import fs from "fs";
 const pdf = require("html-pdf");
 
 export default async(req: Request, res: Response) => {
-  const name: string = typeof req.query.name === "string" ? req.query.name : "";
-  const type = typeof req.query.type === "string" ? req.query.type : "";
-  const id = typeof req.query.id === "string" ? req.query.id : "";
+  res.json({mes: "hola"})
+  // const name: string = typeof req.query.name === "string" ? req.query.name : "";
+  // const type = typeof req.query.type === "string" ? req.query.type : "";
+  // const id = typeof req.query.id === "string" ? req.query.id : "";
 
-    const html = fs.readFileSync(__dirname + "/views/index.html", "utf8");
+  //   const html = fs.readFileSync(__dirname + "/views/index.html", "utf8");
 
-    const reHtml = html.replace('{{userName}}', name)
-                       .replace('{{document_type}}', type)
-                       .replace('{{document}}', id);
+  //   const reHtml = html.replace('{{userName}}', name)
+  //                      .replace('{{document_type}}', type)
+  //                      .replace('{{document}}', id);
 
-    pdf.create(reHtml, {
-        "type": "PDF",
-        "renderDelay": 2000,
-        "orientation": "landscape",
-        "base": "file:///" + __dirname + "\\views\\images\\",
-    })
-        .toStream((err: any, stream: any) => {
-            //    stream.pipe(fs.createWriteStream('../pdf/certification.pdf'))
-            if (err) {
-                return res.send({
-                    status: 500,
-                    msg: "error"
-                })
-            } else {
-                res.statusCode = 200;
+  //   pdf.create(reHtml, {
+  //       "type": "PDF",
+  //       "renderDelay": 2000,
+  //       "orientation": "landscape",
+  //       "base": "file:///" + __dirname + "\\views\\images\\",
+  //   })
+  //       .toStream((err: any, stream: any) => {
+  //           //    stream.pipe(fs.createWriteStream('../pdf/certification.pdf'))
+  //           if (err) {
+  //               return res.send({
+  //                   status: 500,
+  //                   msg: "error"
+  //               })
+  //           } else {
+  //               res.statusCode = 200;
 
-                stream.on('end', () => {
-                    return res.end();
-                })
+  //               stream.on('end', () => {
+  //                   return res.end();
+  //               })
 
-                stream.pipe(res);
-            }
-        })
+  //               stream.pipe(res);
+  //           }
+  //       })
 }
 
 
