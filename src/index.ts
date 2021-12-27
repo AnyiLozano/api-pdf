08c89f99811b8f3960553f33e1ddeb6d85844ad4
AnyiLozano <anyi.lozano2119@gmail.com>
2021-12-27 16:48:50 -0500

    Change

--- src/index.ts
+++ src/index.ts
@@ -9,35 +9,35 @@ export default async(req: Request, res: Response) => {
   // const type = typeof req.query.type === "string" ? req.query.type : "";
   // const id = typeof req.query.id === "string" ? req.query.id : "";
 
-  //   const html = fs.readFileSync(__dirname + "/views/index.html", "utf8");
+    const html = fs.readFileSync(__dirname + "/views/index.html", "utf8");
 
   //   const reHtml = html.replace('{{userName}}', name)
   //                      .replace('{{document_type}}', type)
   //                      .replace('{{document}}', id);
 
-  //   pdf.create(reHtml, {
-  //       "type": "PDF",
-  //       "renderDelay": 2000,
-  //       "orientation": "landscape",
-  //       "base": "file:///" + __dirname + "\\views\\images\\",
-  //   })
-  //       .toStream((err: any, stream: any) => {
-  //           //    stream.pipe(fs.createWriteStream('../pdf/certification.pdf'))
-  //           if (err) {
-  //               return res.send({
-  //                   status: 500,
-  //                   msg: "error"
-  //               })
-  //           } else {
-  //               res.statusCode = 200;
+    pdf.create(html, {
+        "type": "PDF",
+        "renderDelay": 2000,
+        "orientation": "landscape",
+        "base": "file:///" + __dirname + "\\views\\images\\",
+    })
+        .toStream((err: any, stream: any) => {
+            //    stream.pipe(fs.createWriteStream('../pdf/certification.pdf'))
+            if (err) {
+                return res.send({
+                    status: 500,
+                    msg: "error"
+                })
+            } else {
+                res.statusCode = 200;
 
-  //               stream.on('end', () => {
-  //                   return res.end();
-  //               })
+                stream.on('end', () => {
+                    return res.end();
+                })
 
-  //               stream.pipe(res);
-  //           }
-  //       })
+                stream.pipe(res);
+            }
+        })
 }
 
 
